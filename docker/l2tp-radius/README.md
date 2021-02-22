6e84f5c7f92f8354d8ab662307fdb82ec1e7b3d5
vzakharchenko <vzakharchenko@modeln.com>
2021-02-22 15:57:31 +0200

    added installation steps for docker/l2tp-radius

---
+++ docker/l2tp-radius/README.md
@@ -0,0 +1,12 @@
+# Docker image with L2TP (IpSec, RadSec) server including routing and port forwarding
+
+## Documentation
+
+[here](https://github.com/vzakharchenko/l2tp-ipsec-radius-docker)
+
+## Installation
+[create /opt/config.json](https://github.com/vzakharchenko/l2tp-ipsec-radius-docker#configjson-structure)
+```
+sudo apt-get update && sudo apt-get install -y curl
+curl -sSL https://github.com/vzakharchenko/remote-ctrl-gsm/raw/master/docker/l2tp-radius/ubuntu.install| bash
+```
---
+++ docker/l2tp-radius/generateDockerCommands.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+
+const configPath = process.env['CONFIG_PATH'] || '../examples/config.json';
+
+function parseFile(cJson) {
+    let redir = '';
+    let radsec = '';
+    if (cJson.radsec) {
+        if (!fs.existsSync(cJson.radsec.privateKey)) {
+            throw new Error('Radsec Private key does not exist')
+        }
+        if (!fs.existsSync(cJson.radsec.certificateFile)) {
+            throw new Error('Radsec Public key does not exist')
+        }
+        if (!fs.existsSync(cJson.radsec.CACertificateFile)) {
+            throw new Error('Radsec CA key does not exist')
+        }
+        radsec = `-v ${cJson.radsec.privateKey}:${cJson.radsec.privateKey} -v ${cJson.radsec.certificateFile}:${cJson.radsec.certificateFile} -v ${cJson.radsec.CACertificateFile}:${cJson.radsec.CACertificateFile} `
+    }
+    if (cJson.authorizationMap) {
+        if (cJson.authorizationMap.roles) {
+            Object.entries(cJson.authorizationMap.roles).forEach(((kv) => {
+                const value = kv[1];
+                if (value.forwarding) {
+                    value.forwarding.forEach(f => {
+                        redir = redir + `-p ${f.externalPort}:${f.externalPort} `
+                    })
+
+                }
+            }));
+        }
+    }
+    console.log('docker run -d --name=remote-ctrl -p 500:500/udp -p 4500:4500/udp -p 3799:3799/udp ' + redir +
+        `-v ${configPath}:/opt/config.json ${radsec} --privileged --restart=always vassio/remote-ctrl-l2tp-radius:latest`)
+}
+
+const f = fs.readFileSync(configPath, 'utf8');
+
+const configJson = JSON.parse(f);
+parseFile(configJson);
+
