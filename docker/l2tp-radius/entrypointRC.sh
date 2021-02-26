abffd7c70ea42815eda2886224493a3d29b559b8
vzakharchenko <vzakharchenko@modeln.com>
2021-02-26 09:37:12 +0200

    fix

--- docker/l2tp-radius/entrypointRC.sh
+++ docker/l2tp-radius/entrypointRC.sh
@@ -1,2 +1,5 @@
+#!/bin/bash
+
+set -e
 pm2 start `npm root -g`/smartthings-phevctl/smartthings-phevctl.js
 entrypoint.sh
--- docker/pptp/entrypointRC.sh
+++ docker/pptp/entrypointRC.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
+
+set -e
+
 pm2 start `npm root -g`/smartthings-phevctl/smartthings-phevctl.js
 entrypoint.sh
