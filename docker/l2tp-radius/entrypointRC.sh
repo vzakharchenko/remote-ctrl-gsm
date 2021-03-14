06f6b46e5239702e49254ba6d63527db4d72bd7c
vzakharchenko <vzakharchenko@modeln.com>
2021-03-14 20:49:19 +0200

    changed script

--- docker/l2tp-radius/entrypointRC.sh
+++ docker/l2tp-radius/entrypointRC.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
-if [[ "x${SMARTTHINGS}" != "xfalse" ]]; then
+if [[ "x${SMARTTHINGS}" = "xtrue" ]]; then
   pm2 start `npm root -g`/smartthings-phevctl/smartthings-phevctl.js
 fi
 /entrypoint.sh
